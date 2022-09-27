884fde2c06b0039d869a9af1ad9e83281b94fd38
Jacob Taylor <100665980+jacvbtaylor@users.noreply.github.com>
2022-09-27 14:22:13 -0700

    Update nmapscript.sh

--- nmapscript.sh
+++ nmapscript.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #by @jacvbtaylor
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
  
-echo "sed -e '/Discovered/d;/Increasing/d;/closed/d;/Completed/d;/Initiating/d;/Starting/d;/Not/d;/Nmap/d'" > SED && chmod +x SED
-printf "enter the ip\n\n"
+echo "sed -e '/Raw/d;/Read/d;/Discovered/d;/Increasing/d;/closed/d;/Completed/d;/Initiating/d;/Starting/d;/Not/d;/Nmap/d'" > SED && chmod +x SED
+printf "input the ip, then press enter\n\n"
 read IP 
 echo "${IP}" > host\
-&& nmap -sn -v -iL host -oG - | awk '/Up$/{print$2}' > livehosts.txt && rm host\
-&& nmap -v -iL livehosts.txt | ./SED >> nmapscan.txt\
-&& nmap -sV -A --script vulners -iL livehost* | ./SED >> vulnscan.txt && rm SED
+&& nmap -sn -v -iL host -oG - | awk '/Up$/{print$2}' > livehosts.txt &&\
+printf "${GREEN}\n\n\n[x]Found live host, scanning now[x]\n\n\n${ENDCOLOR}" && echo "_______________________________" &&\
+rm host && nmap -v -iL livehosts.txt | ./SED >> scan.txt && printf "${RED}\n\n~~~~Port scan is now complete~~~~\n\n${ENDCOLOR}" \
+&& cat scan.txt && printf "${GREEN}\n\n\n~~~~Checking for vulnerabilities~~~~\n\n\n${ENDCOLOR}" &&\
+nmap -sV -A --script=vuln -iL livehost* | ./SED >> vuln.txt &&\
+rm SED && printf "${RED}\n\n\n[x]vulnerability scan is now complete[x]\nPlease review the vuln.txt file.${ENDCOLOR}"
